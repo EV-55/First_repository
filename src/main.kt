c34b2b2bfc4e3e6d10decc8e7ef995582c73e135
EV-55 <77371444+EV-55@users.noreply.github.com>
2021-01-13 11:26:57 +0700

    Calculator3.1.kt

--- src/main.kt
+++ src/main.kt
@@ -1,15 +1,16 @@
 fun main()
+// Программа написана Bazilevs Technology. 
 {
-    println("Вас приветствует программа Калькулятор 3000.")
-    print("Введите первое число")
+    println("Вас приветствует программа Калькулятор 3.1.")
+    print("Введите первое число:")
     var num1 = readLine().toString().toDouble()
-    print("Введите второе число")
+    print("Введите второе число:")
     var num2 = readLine().toString().toDouble()
     print("Введите операцию:")
     var operation = readLine()
 
-when(operation)
-{
+    when(operation)
+    {
         "+" -> println(num1+num2)
         "-" -> println(num1-num2)
         "/" -> println(num1/num2)
@@ -17,8 +18,9 @@ when(operation)
         ":" -> println(num1/num2)
         "х" -> println(num1*num2)
         "x" -> println(num1*num2)
-    else -> println("Ошибка. Повторите попытку.")
-}
+        else -> println("Ошибка. Повторите попытку.")
+    }
 
 }
 // Используйте IntelliJ IDEA от JetBrains.
+// Язык программирования - Kotlin.
